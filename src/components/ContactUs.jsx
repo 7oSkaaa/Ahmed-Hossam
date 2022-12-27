4d93eba15c962a952a9a2e68c2466d2c8931c8d5
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-12-27 23:05:28 +0200

    refactor: remaking some elements

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,60 +1,56 @@
-import React, { Component } from "react";
-export default class ContactUs extends Component {
-    render() {
-        let resumeData = this.props.resumeData;
-        return (
-            <section id="contact">
-                <div className="row section-head">
-                    <div className="ten columns">
-                        <p className="lead">
-                            Feel free to contact me for any work or suggestions
-                            below
-                        </p>
-                    </div>
+import React from "react";
+export default function ContactUs({ resumeData }) {
+    return (
+        <section id="contact">
+            <div className="row section-head">
+                <div className="ten columns">
+                    <p className="lead">
+                        Feel free to contact me for any work or suggestions
+                        below
+                    </p>
                 </div>
-                <div className="row">
-                    <aside
-                        className="eigth columns footer-widgets"
-                        style={{ width: "100%" }}
+            </div>
+            <div className="row">
+                <aside
+                    className="eigth columns footer-widgets"
+                    style={{ width: "100%" }}
+                >
+                    <ul
+                        className="widget"
+                        style={{
+                            ...{ display: "flex" },
+                            ...{ justifyContent: "space-evenly" },
+                            ...{ width: "100%" },
+                            ...{ flexWrap: "wrap" },
+                            ...{ gap: "5%" },
+                            ...{ alignItems: "baseline" },
+                        }}
                     >
-                        <ul
-                            className="widget"
-                            style={{
-                                ...{ display: "flex" },
-                                ...{ justifyContent: "space-evenly" },
-                                ...{ width: "100%" },
-                                ...{ flexWrap: "wrap" },
-                                ...{ gap: "5%" },
-                                ...{ alignItems: "baseline" }
-                              
-                            }}
-                        >
-                            {resumeData.inTouch &&
-                                resumeData.inTouch.map((item) => {
-                                    return (
-                                        <li
-                                            key={item.name}
-                                            style={{ listStyleType: "none" }}
+                        {resumeData.inTouch &&
+                            resumeData.inTouch.map((item, idx) => {
+                                return (
+                                    <li
+                                        key={idx}
+                                        style={{ listStyleType: "none" }}
+                                    >
+                                        <a
+                                            href={item.url}
+                                            target="_blank"
+                                            rel="noreferrer"
                                         >
-                                            <a
-                                                href={item.url}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                <img
-                                                    src={item.icon}
-                                                    alt={item.name}
-                                                    width={50}
-                                                    height={20}
-                                                />
-                                            </a>
-                                        </li>
-                                    );
-                                })}
-                        </ul>
-                    </aside>
-                </div>
-            </section>
-        );
-    }
+                                            <img
+                                                src={item.icon}
+                                                alt={item.name}
+                                                width={50}
+                                                height={20}
+                                            />
+                                        </a>
+                                    </li>
+                                );
+                            })}
+                    </ul>
+                </aside>
+            </div>
+        </section>
+    );
 }
