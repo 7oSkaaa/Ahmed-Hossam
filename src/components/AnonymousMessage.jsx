a086481718761dedcb16b6c29728607c82d6a93d
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-12-31 19:03:03 +0200

    refactor: making some styles

--- src/components/AnonymousMessage.jsx
+++ src/components/AnonymousMessage.jsx
@@ -1,8 +1,14 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { Alert, AlertTitle } from '@mui/material';
 
 export default function AnonymousMessage() {
     const form = useRef();
+    const [isSent, setIsSent] = useState(false);
+    const [isShow, setIsShow] = useState(false);
+    const [message, setMessage] = useState('');
+    const [text, setText] = useState('');
+
 
     const sendEmail = (e) => {
         e.preventDefault();
@@ -16,10 +22,15 @@ export default function AnonymousMessage() {
             )
             .then(
                 (result) => {
-                    console.log(result.text);
+                    setIsSent(true);
+                    setIsShow(true);
+                    document.getElementById('anonymous_textarea').innerHTML = '';
+                    setText('');
                 },
                 (error) => {
-                    console.log(error.text);
+                    setIsSent(false);
+                    setIsShow(true);
+                    setMessage(error.text)
                 }
             );
     };
@@ -31,8 +42,15 @@ export default function AnonymousMessage() {
                 name="message"
                 className="anonymous_textarea"
                 placeholder="Write your message here ..."
+                id="anonymous_textarea"
+                onChange={event => setText(event.target.value)}
+                value={text}
             />
             <input type="submit" value="Send" className="anonymous_button" />
+            {
+                isShow ? isSent ? <Alert variant="filled" severity={'success'} onClose={() => {setIsShow(false)}}><AlertTitle>Success</AlertTitle>
+                Your message has been sent successfully</Alert> : <Alert variant="filled" severity={'error'} onClose={() => {setIsShow(false)}}><AlertTitle>Failed</AlertTitle>{message}</Alert> : null
+            }
         </form>
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,6 @@ export default function Footer({ resumeData }) {
     return (
         <footer>
             <div className="row">
-                <div className="twelve columns">
-                    <ul className="social-links">
-                        {resumeData.socialLinks &&
-                            resumeData.socialLinks.map((item, idx) => {
-                                return (
-                                    <li key={idx}>
-                                        <a href={item.url}>
-                                            <i className={`${item.className}`} />
-                                        </a>
-                                    </li>
-                                );
-                            })}
-                    </ul>
-                </div>
                 <div id="go-top">
                     <a
                         className="smoothscroll"
