1d1c038b318dcee514ae69527b05eab7551adb53
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-01-29 21:26:21 +0200

    refactor: delete loader

--- src/App.jsx
+++ src/App.jsx
@@ -7,40 +7,21 @@ import Testimonials from "./components/Testimonials";
 import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
 import AnonymousMessage from "./components/AnonymousMessage";
-import MoonLoader from 'react-spinners/MoonLoader'
+import resumeData from "./components/resumeData";
 
 function App() {
-    
-    const [fetched, setFetched] = React.useState(false);
-    let [resumeData, setResumeData] = React.useState({});
-
-    React.useEffect(() => {
-        fetch(process.env.REACT_APP_BIN_URL)
-            .then((res) => res.json())
-            .then((res) => {
-                setResumeData(res);
-                setFetched(true);
-            });
-    }, [resumeData, fetched]);
-
-    if(fetched){
-        return (
-            <div className="App">
-                <Header resumeData={resumeData} />
-                <About resumeData={resumeData} />
-                <Resume resumeData={resumeData} />
-                <Projects resumeData={resumeData} />
-                <AnonymousMessage />
-                <ContactUs resumeData={resumeData} />
-                <Testimonials resumeData={resumeData} />
-                <Footer resumeData={resumeData} />
-            </div>
-        );
-    }else {
-        return (
-            <div className='Loader_Container'><MoonLoader className='Loader' color={'#00CCCC'} loading={!fetched} size={120}/></div>
-        );
-    }
+    return (
+        <div className="App">
+            <Header resumeData={resumeData} />
+            <About resumeData={resumeData} />
+            <Resume resumeData={resumeData} />
+            <Projects resumeData={resumeData} />
+            <AnonymousMessage />
+            <ContactUs resumeData={resumeData} />
+            <Testimonials resumeData={resumeData} />
+            <Footer resumeData={resumeData} />
+        </div>
+    );
 }
 
 export default App;
